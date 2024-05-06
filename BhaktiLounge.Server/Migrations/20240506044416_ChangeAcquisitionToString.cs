3843bfc3c11d0aba43fbca49a88c08ad90dcf8bd
jinanme <jinanme@myvuw.ac.nz>
2024-05-06 20:11:46 +1200

    BE: changed the acquisition type, commented out some unused converter.

---
+++ BhaktiLounge.Server/Migrations/20240506044416_ChangeAcquisitionToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BhaktiLounge.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeAcquisitionToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Acquisition",
+                table: "Customer",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Acquisition",
+                table: "Customer",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
