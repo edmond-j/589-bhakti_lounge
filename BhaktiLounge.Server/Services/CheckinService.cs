58d03eeeecb51fee208a8552caded1782495e7ba
jinanme <jinanme@myvuw.ac.nz>
2024-05-07 12:13:14 +1200

    BE: add today's checkin API

--- BhaktiLounge.Server/Services/CheckinService.cs
+++ BhaktiLounge.Server/Services/CheckinService.cs
@@ -2,46 +2,35 @@
 using BhaktiLounge.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 
 namespace BhaktiLounge.Server.Services;
 
-public class CheckinService : ICheckinService
-{
-    
+public class CheckinService : ICheckinService {
     private readonly ApplicationDbContext _context;
-    public CheckinService(ApplicationDbContext context)
-    {
-        _context = context;  
+
+    public CheckinService(ApplicationDbContext context) {
+        _context = context;
     }
-    
-    public async Task<List<Checkin>> GetAllCheckins()
-    {
-        var checkins = 
+
+    public async Task<List<Checkin>> GetAllCheckins() {
+        var checkins =
             await _context.Checkin
                 .Include(c => c.Customer)
                 .OrderBy(a => a.Id)
                 .ToArrayAsync();
         return checkins.ToList();
     }
-    
-    public async Task<Boolean> AddCheckin(Checkin checkin)
-    {   
+
+    public async Task<Boolean> AddCheckin(Checkin checkin) {
         _context.Checkin.Add(checkin);
-        try
-        {
+        try {
             var affectedRows = await _context.SaveChangesAsync();
-            return affectedRows==1; // Returns the number of entities written to the database
-        }
-        catch (DbUpdateException ex)
-        {
+            return affectedRows == 1; // Returns the number of entities written to the database
+        } catch (DbUpdateException ex) {
             throw new InvalidOperationException("Error saving the activity to the database.", ex);
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
             throw new Exception("An unexpected error occurred.", ex);
         }
     }
-    
-    
-    
 }
