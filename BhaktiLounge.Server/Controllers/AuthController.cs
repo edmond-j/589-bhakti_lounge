bf8b1410ff621f76e9f479101141477e79bdd463
Zhefu Zhang <41334845+arcatva@users.noreply.github.com>
2024-05-10 02:13:08 +1200

    refactor: rename "register", "login", "logout" to "signup", "signin" and "signout"

--- BhaktiLounge.Server/Controllers/AuthController.cs
+++ BhaktiLounge.Server/Controllers/AuthController.cs
@@ -27,8 +27,8 @@ public class AuthController : ControllerBase
         _userManager = userManager;
     }
 
-    [HttpPost("register")]
-    public async Task<IActionResult> Register([FromBody] RegisterModel userModel)
+    [HttpPost("signup")]
+    public async Task<IActionResult> Signup([FromBody] SignupModel userModel)
     {
         var user = new Admin()
         {
@@ -51,8 +51,8 @@ public class AuthController : ControllerBase
         return BadRequest(resultCreateUser.Errors);
     }
 
-    [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] UserModel userModel)
+    [HttpPost("signin")]
+    public async Task<IActionResult> Signin([FromBody] UserModel userModel)
     {
         // Find the user by username
         var user = await _userManager.FindByNameAsync(userModel.UserName);
