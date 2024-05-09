7d50f7111465a9befbb6dd05e2c31fb5b64083f2
Zhefu Zhang <41334845+arcatva@users.noreply.github.com>
2024-05-10 02:13:38 +1200

    feat: try to add auth util in front-end

---
+++ bhaktilounge.client/src/services/auth.service.js
@@ -0,0 +1,69 @@
+﻿const API_URL = "/api/v1/auth/";
+
+// Function to register a new user
+const signup = (username, email, password, firstname, lastname) => {
+	return fetch(API_URL + "signup", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body:
+			JSON.stringify({
+				username: username,
+				email: email,
+				password: password,
+				firstname: firstname,
+				lastname: lastname
+			}),
+	})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		})
+		.catch(error => {
+			console.error('There was a problem with the fetch operation:', error);
+		});
+};
+
+// Function to login a user
+const signin = (username, password) => {
+	return fetch(API_URL + "signin", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({
+			username,
+			password,
+		}),
+	})
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok');
+			}
+			return response.json();
+		})
+		.then(data => {
+			if (data.accessToken) {
+				localStorage.setItem("user", JSON.stringify(data));
+			}
+			return data;
+		})
+		.catch(error => {
+			console.error('There was a problem with the fetch operation:', error);
+		});
+};
+
+
+const signout = () => {
+	localStorage.removeItem("user");
+};
+
+// Exporting the functions as an object
+export default {
+	signup,
+	signin,
+	signout,
+};
