6d0f634fd6c171ee964539e3fc3c4c5a5f2f3fc3
jinanme <jinanme@myvuw.ac.nz>
2024-05-20 13:25:40 +1200

    fix: register page navigate, unable to register because of check box value

--- bhaktilounge.client/src/pages/Register.jsx
+++ bhaktilounge.client/src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import Header from '../components/Header.jsx';
 
 const Register = () => {
@@ -26,87 +26,111 @@ const Register = () => {
             notification: notification,
         };
         const requestOptions = {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(newData),
         };
 
         console.log(requestOptions);
-        fetch("/api/v1/Customer", requestOptions)
+        fetch('/api/v1/Customer', requestOptions)
             .then((response) => response.json())
             .then((data) => {
-                console.log("Update Successful:", data);
-                alert(FirstName + LastName + " has been registered!")
+                console.log('Update Successful:', data);
+                alert(FirstName + LastName + ' has been registered!');
             })
-            .catch((error) => console.error("Error:", error));
-        const customerName = FirstName + " " + LastName;
-        navigate('check/check-in', { state: { FirstName } });
+            .catch((error) => console.error('Error:', error));
+        const customerName = FirstName + ' ' + LastName;
+        navigate('/check/check-in', { state: { FirstName } });
     };
 
     return (
         <div className='page-container'>
             <Header pageNumber={2} />
-            <div className="main-content">
+            <div className='main-content'>
                 <h2>New Customer</h2>
                 <form onSubmit={handleSubmit}>
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label htmlFor='firstname'>First Name</label>
-                        <input id="firstname" type="text" required onChange={(e) => setFirstName(e.target.value)}></input>
-                        <label htmlFor='lastname'>Last Name</label>
-                        <input id="lastname" type="text" required onChange={(e) => setLastName(e.target.value)}></input>
-                    </div>
-                    <div className="form-group" >
-                        <label htmlFor="email">Email</label>
                         <input
-                            id="email"
-                            type="email"
+                            id='firstname'
+                            type='text'
+                            required
+                            onChange={(e) => setFirstName(e.target.value)}></input>
+                        <label htmlFor='lastname'>Last Name</label>
+                        <input
+                            id='lastname'
+                            type='text'
+                            required
+                            onChange={(e) => setLastName(e.target.value)}></input>
+                    </div>
+                    <div className='form-group'>
+                        <label htmlFor='email'>Email</label>
+                        <input
+                            id='email'
+                            type='email'
                             value={email}
                             onChange={(e) => setEmail(e.target.value)}
                             placeholder='Enter an email address'
                             required
                         />
                     </div>
-                    <div className="form-group" >
-                        <label htmlFor="pronouns">Pronouns</label>
-                        <select id="pronouns" value={gender} name="pronouns" required
+                    <div className='form-group'>
+                        <label htmlFor='pronouns'>Pronouns</label>
+                        <select
+                            id='pronouns'
+                            value={gender}
+                            name='pronouns'
+                            required
                             onChange={(e) => setGender(e.target.value)}>
-                            <option value="" disabled hidden>Choose one</option>
-                            <option value="he">He/Him</option>
-                            <option value="she">She/Her</option>
-                            <option value="they">They/Them</option>
-                            <option value="other">Other</option>
+                            <option value='' disabled hidden>
+                                Choose one
+                            </option>
+                            <option value='he'>He/Him</option>
+                            <option value='she'>She/Her</option>
+                            <option value='they'>They/Them</option>
+                            <option value='other'>Other</option>
                         </select>
                     </div>
 
-                    <div className="form-group" >
-                        <label htmlFor="channel">How did you hear about us?</label>
-                        <select id="channel" value={acquisition} name="channel" required
+                    <div className='form-group'>
+                        <label htmlFor='channel'>How did you hear about us?</label>
+                        <select
+                            id='channel'
+                            value={acquisition}
+                            name='channel'
+                            required
                             onChange={(e) => setAcquisition(e.target.value)}>
-                            <option value="" disabled hidden>Choose one</option>
-                            <option value="Facebook">Facebook</option>
-                            <option value="Instagram">Instagram</option>
-                            <option value="WordOfMouth">Word of Mouth</option>
-                            <option value="Flyer">Flyer</option>
-                            <option value="Poster">Poster</option>
-                            <option value="GoogleSearch">Google Search</option>
-                            <option value="Eventbrite">Eventbrite</option>
-                            <option value="EventFinder">EventFinder</option>
-                            <option value="Humanitix">Humanitix</option>
-                            <option value="BhaktiLoungeWebsite">Bhakti Lounge Website</option>
-                            <option value="SelfDiscover">Self Discovery</option>
-                            <option value="Other">Other</option>
+                            <option value='' disabled hidden>
+                                Choose one
+                            </option>
+                            <option value='Facebook'>Facebook</option>
+                            <option value='Instagram'>Instagram</option>
+                            <option value='WordOfMouth'>Word of Mouth</option>
+                            <option value='Flyer'>Flyer</option>
+                            <option value='Poster'>Poster</option>
+                            <option value='GoogleSearch'>Google Search</option>
+                            <option value='Eventbrite'>Eventbrite</option>
+                            <option value='EventFinder'>EventFinder</option>
+                            <option value='Humanitix'>Humanitix</option>
+                            <option value='BhaktiLoungeWebsite'>Bhakti Lounge Website</option>
+                            <option value='SelfDiscover'>Self Discovery</option>
+                            <option value='Other'>Other</option>
                         </select>
                     </div>
-                    <div className="form-group">
+                    <div className='form-group'>
                         <label htmlFor='notification'>Enable Notification?</label>
-                        <input id="notification" type="checkbox" onChange={(e) => setNotification(e.target.value)}></input>
-                        
+                        <input
+                            id='notification'
+                            type='checkbox'
+                            onChange={(e) => setNotification(e.target.checked)}></input>
                     </div>
                     <div className='button-container'>
-                        <button className="tw-btn" type="submit">Sign Up</button>
+                        <button className='tw-btn' type='submit'>
+                            Sign Up
+                        </button>
                         {/* <button type="submit" onClick={backButton}>Back</button> */}
-                        <Link to="/check/check-in" >
-                        <button className="tw-btn">Back</button>
+                        <Link to='/check/check-in'>
+                            <button className='tw-btn'>Back</button>
                         </Link>
                     </div>
                 </form>
